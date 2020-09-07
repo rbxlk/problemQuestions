0d7b3b3d88ee3b495a34bf908fef91f4946ac3d6
Robert Luck <rl19aaj@herts.ac.uk>
2020-09-07 14:07:36 +0100

    Update directory structure
    Add linkedList data structure practice

---
+++ data-structures/linkedList.js
@@ -0,0 +1,46 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+const head = Symbol("head");
+class LinkedList {
+    constructor() {
+        this[head] = null;
+    }
+
+    add(data) {
+        const newNode = new Node(data);
+        if (this[head] === null) {
+            this[head] = newNode;
+        } else {
+            let current = this[head];
+
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+
+    countNodes() {
+        let count = 1;
+        let current = this[head];
+        while (current.next != null) {
+            current = current.next;
+            count++;
+        }
+        return count;
+    }
+}
+
+newList = new LinkedList();
+
+newList.add('yellow');
+newList.add('blue');
+newList.add('red');
+
+console.log(newList.countNodes());
